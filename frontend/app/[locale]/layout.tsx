aa4cd7d42c90e971fb2660ce7a234fef15810df4
Devomel <omellwork@gmail.com>
2025-12-03 12:04:20 +0200

    new page for slug

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
    variable: "--font-geist-sans",
@@ -35,7 +36,10 @@ export default async function LocaleLayout({
          style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
       >
          <Navbar locale={locale} />
-         {children}
+         <main style={{ flex: 1 }}>
+            {children}
+         </main>
+         <Footer locale={locale} />
       </div>
    );
 }
--- frontend/app/[locale]/news/[slug]/page.tsx
+++ frontend/app/[locale]/news/[slug]/page.tsx
@@ -1,3 +1,4 @@
+
 import { fetchAPI } from "@/app/lib/api";
 
 export default async function NewsItemPage({
@@ -5,36 +6,46 @@ export default async function NewsItemPage({
 }: {
    params: Promise<{ locale: string; slug: string }>;
 }) {
-   const resolvedParams = await params; // розпаковуємо проміс
+   const resolvedParams = await params;
 
-   console.log(resolvedParams);
-   const res = await fetchAPI(`/api/news?filters[Slug][$eq]=${resolvedParams.slug}`,
+   const res = await fetchAPI(
+      `/api/news?filters[Slug][$eq]=${resolvedParams.slug}`,
       resolvedParams.locale
    );
-   console.log("item1111", res);
-
 
    if (!res.data || res.data.length === 0) {
-      return <div>Not found</div>;
+      return <div className="text-center text-xl font-semibold mt-10">Матеріал не знайдено</div>;
    }
 
    const item = res.data[0];
 
    return (
-      <article>
-         <h1>{item.Title}</h1>
-         {renderRichText(item.Content)}
-      </article>
+      <>
+         <h1 className="text-4xl font-bold mb-4 text-center">{item.Title}</h1>
+
+         {
+            item.Cover?.url && (
+               <img
+                  src={item.Cover.url}
+                  alt={item.Cover.alternativeText || "cover"}
+                  className="w-full rounded-xl mb-8 border border-gray-200"
+               />
+            )
+         }
+
+         <div className="rounded-2xl border border-gray-200 shadow p-6 prose prose-lg max-w-none">
+            {renderRichText(item.Content)}
+         </div>
+      </>
+
+
    );
 }
+
 function renderRichText(blocks: any[]): React.ReactNode {
    return blocks.map((block, i) => {
       if (block.type === "paragraph") {
-         return (
-            <p key={i}>
-               {block.children.map((child: any, j: number) => child.text)}
-            </p>
-         );
+         return <p key={i}>{block.children.map((child: any, j: number) => child.text)}</p>;
       }
       return null;
    });
