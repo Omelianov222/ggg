2471381e68f4a9a16be57b50929c5867e86c5808
Devomel <omellwork@gmail.com>
2026-03-13 13:01:27 +0200

    update contentSecurityPolicy

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,36 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+   'strapi::logger',
+   'strapi::errors',
+   {
+      name: 'strapi::security',
+      config: {
+         contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+               'connect-src': ["'self'", 'https:'],
+               'img-src': [
+                  "'self'",
+                  'data:',
+                  'blob:',
+                  'https://market-assets.strapi.io',
+                  'https://res.cloudinary.com',  // ← додаємо Cloudinary
+               ],
+               'media-src': [
+                  "'self'",
+                  'data:',
+                  'blob:',
+                  'https://res.cloudinary.com',  // ← для відео/аудіо
+               ],
+               upgradeInsecureRequests: null,
+            },
+         },
+      },
+   },
+   'strapi::cors',
+   'strapi::poweredBy',
+   'strapi::query',
+   'strapi::body',
+   'strapi::session',
+   'strapi::favicon',
+   'strapi::public',
 ];
