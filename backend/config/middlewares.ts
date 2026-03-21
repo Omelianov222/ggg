7a4eb8aa9d4dc6e655339ab861f4657ac3af41ff
Devomel <omellwork@gmail.com>
2026-03-21 14:45:39 +0200

    remove cors for test

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,29 +1,7 @@
 export default [
    'strapi::logger',
    'strapi::errors',
-   {
-      name: 'strapi::security',
-      config: {
-         contentSecurityPolicy: {
-            useDefaults: true,
-            // directives: {
-            //    'img-src': [
-            //       "'self'",
-            //       'data:',
-            //       'blob:',
-            //       'https://market-assets.strapi.io',
-            //       'https://res.cloudinary.com',
-            //    ],
-            //    'media-src': [
-            //       "'self'",
-            //       'data:',
-            //       'blob:',
-            //       'https://res.cloudinary.com',
-            //    ],
-            // },
-         },
-      },
-   },
+   'strapi::security',
    'strapi::cors',
    'strapi::poweredBy',
    'strapi::query',
